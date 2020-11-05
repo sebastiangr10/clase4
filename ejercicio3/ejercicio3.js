062f355113167a1324ec2fae9a4a453ba4382a3f
sebastiangr10 <sebastiangr1091@gmail.com>
2020-11-05 09:10:53 -0500

    ejercicio3

--- ejercicio3/ejercicio3.js
+++ ejercicio3/ejercicio3.js
@@ -4,7 +4,14 @@ let ahorro
 
 
 sueldo =Number(prompt("ingrese sueldo mensual"))
-semana= (sueldo/4)
+
+
+function ahorrosemanal(sueldo){
+    semana=(sueldo/4)*0.15
+    return semana
+}
+alert("el ahorro semanal es " + ahorrosemanal(sueldo) + "$")
+
 function ahorroanual(sueldo){
     ahorro=sueldo*12*0.15
     return ahorro
